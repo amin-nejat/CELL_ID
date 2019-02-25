edcca4dade7ab7d9cadb1b6d5968e22d4fa6fdb8
Ev <ev@Evs-Macbook-Pro-2018.local>
2019-02-24 20:44:11 -0500

    First few fixes, check Ev's environment & catching empty file choices.

--- get_params.m
+++ get_params.m
@@ -7,9 +7,16 @@ params.mp.default_window = '[15,15,9]';
 params.mp.default_trunc = '90';
 params.mp.default_k = '200';
 
-params.default_data_directory = '/home/mn2822/Desktop/WormAutoID/data';
-params.fiji_lib_folder = '/home/mn2822/Desktop/WormTracking/libraries/Fiji.app';
-params.mij_path = '/home/mn2822/Desktop/WormTracking/libraries/Fiji.app/mij.jar';
+% !!! ADDED FOR MY OWN CONVENIENCE GET RID OF THIS LATER :)
+if strcmpi('ev', getenv('USER'))
+    params.default_data_directory = '/Users/ev/Documents';
+    params.fiji_lib_folder = '/Applications/Fiji.app';
+    params.mij_path = '/Users/ev/Documents/Github/CELL_ID/data handling/mij.jar';
+else % Amin's setup.
+    params.default_data_directory = '/home/mn2822/Desktop/WormAutoID/data';
+    params.fiji_lib_folder = '/home/mn2822/Desktop/WormTracking/libraries/Fiji.app';
+    params.mij_path = '/home/mn2822/Desktop/WormTracking/libraries/Fiji.app/mij.jar';
+end
 
 end
 
