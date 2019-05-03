d4916e3f10028fe01d391d1acf9af18fddab32e8
Erdem Varol <evarol@gmail.com>
2019-05-03 18:23:29 -0400

    Revert "added model 3 for deployment"
    This reverts commit 3742b4f6c348d00f414a3dbc88ccfe857c0999a2.

--- get_params.m
+++ get_params.m
@@ -12,9 +12,6 @@ params.mp.max_eig_thresh = '10';
 if strcmpi('ev', getenv('USER'))
     params.default_data_directory = '/Users/ev/Documents/Github';
     params.fiji_lib_folder = '/Applications/Fiji.app';
-elseif strcmpi('Erdem',getenv('USERNAME'))
-    params.default_data_directory = 'C:\Users\Erdem\Documents\GitHub';
-    params.fiji_lib_folder = 'C:\Users\Erdem\Dropbox\Projects\worm\Fiji.app';
 else % Amin's setup.
     params.default_data_directory = '/home/mn2822/Desktop/WormAutoID/data';
     params.fiji_lib_folder = '/home/mn2822/Desktop/WormTracking/libraries/Fiji.app';
