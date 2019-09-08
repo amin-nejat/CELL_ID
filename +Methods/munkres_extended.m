5d1b02c19d87438a933aa763a4f68a415cc5d3d9
gem2131@columbia.edu <gem2131@columbia.edu>
2019-09-08 02:07:14 -0400

    false positive detector

---
+++ +Methods/munkres_extended.m
@@ -0,0 +1,17 @@
+function [P, cost, P_full] = munkres_extended(Cost, p, n1)
+
+% Solves an assignment problem, but accounts for possible n1 false
+% positives, so each detect is a false positive with probability p/n1.
+n2 = size(Cost,2);
+p2 = p/n1;
+p1 = (1-p)/n2;
+Cost = Cost - log(p1);
+
+
+Cost(:,n2+1:n2+n1) = -log(p2);
+
+
+[P, cost] = munkres(Cost);
+P_full = P;
+P= P_full(:,1:n2);
+
