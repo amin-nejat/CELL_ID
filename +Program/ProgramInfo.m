ed014276cbce65a10f3c839528992ce45cfa725b
Eviatar Yemini <ev@cs.columbia.edu>
2021-10-10 16:49:00 +0200

    Bumped to 1.7. In last update, I use only the marked z plane to compute neuron GFP intensity. Going off Z leads to spurious measures.

--- +Program/ProgramInfo.m
+++ +Program/ProgramInfo.m
@@ -4,7 +4,7 @@ classdef ProgramInfo
     % Program constants.
     properties (Constant, Access = public)
         name = 'NeuroPAL Auto ID'; % program name
-        version = 1.6; % software version
+        version = 1.7; % software version
         version_URL = 'https://raw.githubusercontent.com/amin-nejat/CELL_ID/master/version.info';
         %website_URL = 'http://hobertlab.org/neuropal/';
         website_URL = 'http://yeminilab.com/neuropal/';
